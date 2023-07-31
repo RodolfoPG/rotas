dc49a980d8a899314db59ec423dd7ac8d40da58b
Rodolfo PG <rodolfopgriao@gmail.com>
2023-07-31 18:45:57 -0300

    Rotas Filhas: desenvolvendo as telas

--- src/app/alunos/alunos.module.ts
+++ src/app/alunos/alunos.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+
 import { AlunosComponent } from './alunos.component';
 import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 import { AlunosRoutingModule } from './alunos-routing.module';
+import { AlunosService } from './alunos.service';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -12,7 +20,14 @@ import { AlunosRoutingModule } from './alunos-routing.module';
   declarations: [AlunosComponent, AlunoFormComponent, AlunoDetalheComponent],
   imports: [
     CommonModule,
-    AlunosRoutingModule
-  ]
+    FormsModule,
+    AlunosRoutingModule,
+    MatListModule,
+    MatGridListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
+  providers: [AlunosService]
 })
 export class AlunosModule { }

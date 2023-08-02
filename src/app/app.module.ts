07ed525765a2f883c5b31aefc13eb7b0d0242e69
Rodolfo PG <rodolfopgriao@gmail.com>
2023-08-02 14:45:09 -0300

    Rotas: Tela de Login e como não mostrar o Menu (NavBar)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from './login/auth.service';
+import { FormsModule } from '@angular/forms';
 // import { CursosModule } from './cursos/cursos.module';
 // import { AlunosModule } from './alunos/alunos.module';
 //import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
@@ -34,10 +39,15 @@ import { MatListModule } from '@angular/material/list';
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
     // CursosModule,
     // AlunosModule,
     AppRoutingModule
   ],
+  providers: [AuthService],
   //providers: [CursosService],
   bootstrap: [AppComponent]
 })
---
+++ src/app/login/usuario.ts
@@ -0,0 +1,14 @@
+export class Usuario {
+    nome!: string;
+    senha!: string;
+}
+
+
+// outra maneira de fazer:
+
+// export class Usuario { 
+//   constructor(
+//     public nome: string,
+//     public senha: string
+//   ) {}
+// }
